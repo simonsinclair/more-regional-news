e1fc60a3da1f87a419bfb2415be3c37ac8095129
Simon Sinclair <simon@sinclair>
2014-07-03 11:50:45 +0100

    Refer to master branch

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// main.js
+//
+
+"use strict";
+
+(function(w, $, undefined) {
+
+	var carousel = {
+		init: function() {
+			this.bindEvts();
+		},
+		bindEvts: function() {
+
+		}
+	};
+
+	$(function() {
+		carousel.init();
+	});
+})(this, jQuery);
