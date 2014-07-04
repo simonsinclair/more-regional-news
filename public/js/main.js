e0a4f08b5dc07e16b0423aff04f6f15e64585113
Simon Sinclair <simon@sinclair>
2014-07-04 11:34:44 +0100

    Use Mustache template to tidy up carousel

--- public/js/main.js
+++ public/js/main.js
@@ -3,18 +3,33 @@
 
 "use strict";
 
-(function(w, $, undefined) {
+(function(w, $, M, undefined) {
 
+	// Carousel object
 	var carousel = {
-		init: function() {
+		init: function(carouselId) {
+			this.carousel      = $(carouselId);
+
 			this.bindEvts();
+			this.populate();
 		},
 		bindEvts: function() {
 
+		},
+		populate: function() {
+			$.when(
+				$.get('carousel-items.json'),
+				$.get('tpl/carousel-items.tpl')
+			).then(function(data, tpl) {
+				var rendered = M.render(tpl[0], data[0]);
+				$('#js-carousel__items', this.carousel).html(rendered);
+			});
 		}
 	};
 
+	// Init Carousel on DOM ready
 	$(function() {
-		carousel.init();
+		carousel.init('#js-carousel');
 	});
-})(this, jQuery);
+
+})(this, jQuery, Mustache);
