c67fa0eb4989499ff9b2aec6476c77ec199c6e34
Simon Sinclair <simon@sinclair>
2014-07-04 12:06:10 +0100

    Refer to master branch

--- js/main.js
+++ js/main.js
@@ -3,18 +3,42 @@
 
 "use strict";
 
-(function(w, $, undefined) {
+(function(w, $, M, undefined) {
 
+	// Carousel object
 	var carousel = {
-		init: function() {
+		init: function(carouselId) {
+			this.carousel      = $(carouselId);
+
 			this.bindEvts();
+			this.populate();
 		},
 		bindEvts: function() {
-
+			$('#js-carousel__left', this.carousel).on('click', this.pageLeft);
+			$('#js-carousel__right', this.carousel).on('click', this.pageRight);			
+		},
+		pageLeft: function(e) {
+			e.preventDefault();
+			console.log('pageLeft');
+		},
+		pageRight: function(e) {
+			e.preventDefault();
+			console.log('pageRight');
+		},
+		populate: function() {
+			$.when(
+				$.get('carousel-items.json'),
+				$.get('tpl/carousel-items.tpl')
+			).then(function(data, tpl) {
+				var rendered = M.render(tpl[0], data[0]);
+				$('#js-carousel__items', this.carousel).html(rendered);
+			});
 		}
 	};
 
+	// Init Carousel on DOM ready
 	$(function() {
-		carousel.init();
+		carousel.init('#js-carousel');
 	});
-})(this, jQuery);
+
+})(this, jQuery, Mustache);
---
+++ tpl/carousel-items.tpl
@@ -0,0 +1,12 @@
+{{#carousel-items}}
+	<li>
+		<img src="img/carousel/item_01.jpg" alt="" width="" height="" />
+		<div class="item__caption">
+			<p>{{title}}</p>
+			<ul class="item__footer">
+				<li class="item__timestamp">6h ago</li>
+				<li class="item__region">Coventry &amp; Warwickshire</li>
+			</ul>
+		</div>
+	</li>
+{{/carousel-items}}
