683ff5c22eb0bb47bd9a37ebda61a70d6c758f56
Simon Sinclair <simon@sinclair>
2014-07-08 16:12:22 +0100

    Config widget logic and style

--- public/js/core.js
+++ public/js/core.js
@@ -5,14 +5,56 @@
 
 (function(w, $, M, undefined) {
 
-	var core = {
+	var demo = {
+
+		config: {
+			feature: {
+				numItems: 4
+			},
+			smart: {
+				numItems: 6
+			},
+			tablet: {
+				numItems: 6
+			},
+			desktop: {
+				numItems: 8
+			}
+		},
+
 		init: function() {
-			
+			this.device   = $('#js-mrn').data('device');
+			this.$config  = $('#js-config');
+
+			this.bindEvts();
+			this.loadItems();
+		},
+
+		bindEvts: function() {
+			$('.add', this.$config).on('click', $.proxy(this.addItem, this));
+			$('.remove', this.$config).on('click', $.proxy(this.removeItem, this));
+		},
+
+		loadItems: function() {
+
+		},
+
+		addItem: function(e) {
+			e.preventDefault();
+			// ...
+			console.log(e);
+		},
+
+		removeItem: function(e) {
+			e.preventDefault();
+			// ...
+			console.log(e);
 		}
+
 	};
 
 	// Initialise
 	$(function() {
-		core.init();
+		demo.init();
 	});
 })(this, jQuery, Mustache);
