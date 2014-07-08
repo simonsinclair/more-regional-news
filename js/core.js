4831a710d73dd08845a5b4b1533c00099a3158ea
Simon Sinclair <simon@sinclair>
2014-07-08 14:33:16 +0100

    Refer to master branch

---
+++ js/core.js
@@ -0,0 +1,18 @@
+// core.js
+//
+
+"use strict";
+
+(function(w, $, M, undefined) {
+
+	var core = {
+		init: function() {
+			
+		}
+	};
+
+	// Initialise
+	$(function() {
+		core.init();
+	});
+})(this, jQuery, Mustache);
